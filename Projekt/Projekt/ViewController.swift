cac28dec59816870dc391903aca0a2b835609bc1
Stefan Voljc <stefan.voljc@gmail.com>
2015-10-22 13:43:10 +0200

    initial commit

---
+++ Projekt/Projekt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projekt
+//
+//  Created by Stefan Voljc on 22/10/15.
+//  Copyright © 2015 Stefan Voljc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
